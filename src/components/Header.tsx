62ada9eb72578212ec8c7e75cd5ae1ca4ea54cb7
iamcombo <piset2cambo@gmail.com>
2023-01-27 16:19:01 +0700

    - init

---
+++ src/components/Header.tsx
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+
+const Header = () => {
+  const closeMenu = () => {
+
+  };
+
+  const navLinks = (
+		<>
+			<NavLink href="/" closeMenu={closeMenu}>
+				/
+			</NavLink>
+			<NavLink href="/about" closeMenu={closeMenu}>
+				/about
+			</NavLink>
+			<NavLink href="/talk" closeMenu={closeMenu}>
+				/contact-me
+			</NavLink>
+		</>
+	);
+
+  return (
+    <div className="mx-auto max-w-4xl py-10 px-5">
+      <div className="hidden items-center space-x-2 sm:flex">
+        <nav className="flex-1">
+          <ul className="flex space-x-4">{navLinks}</ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
+
+const navlinkClassname =
+	'block py-3 font-mono text-lg dark:hover:text-white no-underline dark:sm:hover:bg-white/10 rounded-md sm:inline-block sm:px-5 sm:text-sm sm:font-normal sm:bg-white/0 sm:hover:bg-neutral-900/5 sm:rounded-full';
+
+function NavLink(props: {
+	children: ReactNode;
+	href: string;
+	closeMenu?: () => void;
+}) {
+	return (
+		<li className="shrink-0">
+			<Link
+				href={props.href}
+				className={navlinkClassname}
+				onClick={props.closeMenu}
+			>
+				{props.children}
+			</Link>
+		</li>
+	);
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,45 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect, useRef } from 'react';
+import { useRouter } from 'next/router';
+import { AnimatePresence, motion } from 'framer-motion';
+import { loadCursor } from '@/utils/cursor';
+import Header from '@/components/Header';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const ballCanvas = useRef<HTMLDivElement>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+		if (typeof window === 'undefined' || !ballCanvas.current) {
+			return;
+		}
+
+		return loadCursor(ballCanvas.current);
+	}, []);
+
+  return (
+    <>
+      {/* Header */}
+      <Header />
+      {/* Main */}
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={router.asPath}
+          initial={{opacity: 0}}
+          animate={{opacity: 1}}
+          exit={{opacity: 0}}
+        >
+          <main className='mx-auto max-w-3xl md:py-24 lg:py-8'>
+            <Component {...pageProps} />
+          </main>
+        </motion.div>
+      </AnimatePresence>
+      {/* Cursor */}
+      <div
+        ref={ballCanvas}
+        className="ball-transitions pointer-events-none fixed z-30 h-6 w-6 rounded-full border border-black bg-transparent opacity-0 shadow-md duration-200 dark:border-white"
+      />
+    </>
+  )
 }
