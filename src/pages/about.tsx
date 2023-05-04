8ff0b519d1ea3491d7f2ab960c3ce8029ce44db5
combo <piset2cambo@gmail.com>
2023-05-04 22:17:51 +0700

    - update about me

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,9 +7,11 @@ const about = () => {
       <p className="text-lg font-light text-neutral-400 leading-8">
         Hi! I&apos;m Piset. I&apos;m a Web developer from Cambodia 🇰🇭. 
         As a Full-Stack Developer with over three years of experience in web development, I have a deep understanding of both front-end and back-end technologies, including blockchain, React/Next.js, and various backend languages.
-        <br />
+      </p>
+      <p className="text-lg font-light text-neutral-400 leading-8">
         In my previous roles, I have had the opportunity to design and develop a variety of web applications using cutting-edge technologies. My experience in web development has allowed me to build a responsive and user-friendly interfaces that provide seamless user experiences. Additionally, my experience in building scalable and secure server-side applications has helped me integrate databases and third-party APIs to deliver high-quality web applications.
-        <br />
+      </p>
+      <p className="text-lg font-light text-neutral-400 leading-8">
         As a Full-Stack Developer, I am passionate about staying up-to-date with the latest web development trends and technologies. I am always looking for new challenges that can help me grow as a developer and expand my knowledge and skills. I am excited about the opportunity to work with a team of talented developers to create innovative and high-quality web applications.
       </p>
       <div className="p-6" />
