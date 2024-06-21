7faeb182ca0bdc5a7554ea64480136a19dac434f
iamcombo <piset2cambo@gmail.com>
2024-06-21 12:54:54 +0700

    refactor: customize progress bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,17 @@ export default function RootLayout({
         <FooterComponent />
 
         <Cursor />
-        <NextTopLoader />
+        <NextTopLoader
+          color="#2299DD"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={false}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+        />
       </body>
     </html>
   );
